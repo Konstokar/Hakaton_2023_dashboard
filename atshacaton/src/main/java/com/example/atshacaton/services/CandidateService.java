7aa7e912974aaa2bbc6a636930e8b26f02582d65
IliaDuhov <dukhov03@mail.ru>
2023-03-17 17:34:29 +0400

    Candidate changed

--- atshacaton/src/main/java/com/example/atshacaton/services/CandidateService.java
+++ atshacaton/src/main/java/com/example/atshacaton/services/CandidateService.java
@@ -4,10 +4,13 @@ import com.example.atshacaton.entities.Candidate;
 import com.example.atshacaton.repositories.CandidateRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CandidateService {
     private final CandidateRepository candidateRepository;
@@ -16,5 +19,25 @@ public class CandidateService {
         return candidateRepository.findAll();
     }
 
+    public Candidate findOne(long id){
+        Optional<Candidate> foundCandidate = candidateRepository.findById(id);
+        return foundCandidate.orElse(null);
+    }
 
+    @Transactional
+    public void save(Candidate candidate){
+        candidateRepository.save(candidate);
+    }
+    @Transactional
+    public void update(long id, Candidate updareCandidate){
+        updareCandidate.setId(id);
+        candidateRepository.save(updareCandidate);
+    }
+    @Transactional
+    public void delete(long id){
+        candidateRepository.deleteById(id);
+    }
+    public Optional<Candidate> getBuSurName(String middleName){
+        return candidateRepository.findByMiddleName(middleName);
+    }
 }
