efaf17b8406122ec4f38ce3d3eae89debe27bc8d
VladKojima <rabochy.q1234567890@yandex.ru>
2023-03-18 11:09:04 +0400

    стартовый макет

--- atshacaton/frontend/src/App.js
+++ atshacaton/frontend/src/App.js
@@ -1,13 +1,13 @@
-import { BrowserRouter, Routes } from 'react-router-dom';
-import logo from './logo.svg';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './style/sass.css';
+import Main from './Main.js';
 
 
 function App() {
   return (
     <BrowserRouter>
     <Routes>
-      
+      <Route path="/" element={<Main />} />
     </Routes>
     </BrowserRouter>
   );
