c393873a7cbb7d9bb80056947b10a6a5cb129afe
iliyagoga <100951861+iliyagoga@users.noreply.github.com>
2023-03-18 15:54:13 +0400

    registr

--- atshacaton/frontend/src/App.js
+++ atshacaton/frontend/src/App.js
@@ -1,9 +1,26 @@
 
-import Main from './container/Main'
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './style/sass.css';
+//import Main from './containers/Main';
+
+import Favorites from './containers/Favorites';
+import Rating from './containers/Rating';
+import Registration from './containers/Registration'
 function App() {
   return (
-    <Main />
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" >
+        <Route path="registration" element={<Registration />}/>
+        <Route path="main" >
+          <Route path="favorites" element={<Favorites />}>
+            <Route path="rating" elements={<Rating />}/>
+            </Route>
+        </Route>
+      </Route>
+    </Routes>
+    </BrowserRouter>
+
   );
 }
 
