c9566a1b1f388133d7672f6a8642a1a30d899729
VladKojima <rabochy.q1234567890@yandex.ru>
2023-03-20 02:45:40 +0400

    Форма регистрации

--- atshacaton/frontend/src/containers/Registration.js
+++ atshacaton/frontend/src/containers/Registration.js
@@ -1,14 +1,32 @@
 export default function Registration(){
     return(<div className="bg">
     <p className="regtitle">Регистрация</p>
-    <form method="POST" action="#" className="reg">
+    <form method="POST" action="http://localhost:8080/auth/registration" className="reg" onSubmit={async e=>{
+        e.preventDefault();
+
+        let body = {};
+
+        for(let input of e.target.getElementsByTagName('input'))
+        {
+            //if(input.type == 'submit') continue;
+            body[input.name] = input.value;
+        }
+
+        console.log(await (await fetch(e.target.action, {
+            method: 'POST',
+            headers: {
+                'Accept' : 'application/json',
+                'Content-type' : 'application/json'
+            },
+            body: JSON.stringify(body)}) ).json())
+    }}>
     <label>
         <p>Имя пользователя:</p>
         <input type="text" required name="username"/>
     </label>
     <label>
         <p>Телефон:</p>
-        <input type="phone"required  name="phone"/>
+        <input type="phone"required  name="phoneNumber"/>
     </label>
     <label>
         <p>Почта:</p>
